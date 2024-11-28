bee8f7935a92ceb08038f2b5ca4540953a33d446
syj6231 <163286295+syj6231@users.noreply.github.com>
2024-11-28 10:46:24 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -5,18 +5,18 @@ const { exec } = require('child_process');
 
 const app = express();
 
-// 업로드 폴더 설정
+
 const UPLOAD_FOLDER = path.join(__dirname, 'uploads');
 const upload = multer({ dest: UPLOAD_FOLDER });
 
-// 정적 파일 경로 설정
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // API: 음악 파일 업로드 및 분석
 app.post('/analyze', upload.single('musicFile'), (req, res) => {
   const filePath = req.file.path;
 
-  // Python 스크립트 실행 (librosa 사용)
+
   const pythonScript = `python analyze_music.py  "${req.file.path}"`;
   exec(pythonScript, (error, stdout, stderr) => {
     if (error) {
@@ -24,13 +24,12 @@ app.post('/analyze', upload.single('musicFile'), (req, res) => {
       return res.status(500).json({ error: 'Error analyzing music file.' });
     }
 
-    // Python 출력값 반환
+    
     const result = JSON.parse(stdout);
     res.json(result);
   });
 });
 
-// 서버 실행
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
